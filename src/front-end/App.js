40a61680972c5c2e59599f8bb19234dc883f0ed0
Lucas Rafael Oliveira dos Santos <lucasrso2005@dinfor.uniceub>
2024-10-18 21:36:19 -0300

    criação do js html e css da primeira tela

---
+++ src/front-end/App.js
@@ -0,0 +1,21 @@
+import React from 'react' ;
+
+import 'src/global.css'
+
+function App() {
+    return (
+        <div id="app">
+            <aside>
+                <strong> FixACar </strong>
+
+                <figure>
+                    <img src='src/static/imgs/img primeira tela.png'></img>
+                </figure>
+
+                <button type='submit'> Login </button>
+
+                <button type='sbumit'> Registrar </button>
+            </aside>
+        </div>
+    )
+}
