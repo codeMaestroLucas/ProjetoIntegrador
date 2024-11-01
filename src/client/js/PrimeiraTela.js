f44c1245e43e5d2bee7cd2652b91e106fd73718b
lucasrso <lucasrso2005@gmail.com>
2024-11-01 17:35:00 -0300

    Primeira tela com botões de registro e login de usuário

---
+++ src/client/js/PrimeiraTela.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function PrimeiraTela() {
+    const handleRegister = () => {
+      alert('Botão de Registrar foi clicado!');
+    };
+  
+    const handleLogin = () => {
+      alert('Botão de Login foi clicado!');
+    };
+  
+    return (
+      <div className="container">
+        <h1>Bem-vindo ao FixACar!</h1>
+        <p>Escolha uma opção:</p>
+        <button className="btn btn-register" onClick={handleRegister}>
+          Registrar
+        </button>
+        <button className="btn btn-login" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+    );
+  }
+  
+
+
+
+export default PrimeiraTela;
